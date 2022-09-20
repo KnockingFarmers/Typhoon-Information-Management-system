63ecfc0fa96ae0744a41e82204ea9dd7ac4f4a5d
根号三 <3237764237@qq.com>
2022-09-20 22:59:41 +0800

    增加用户功能的部分接口

--- typhoon-data-app/src/main/java/com/xxx/typhoon/app/controller/UserController.java
+++ typhoon-data-app/src/main/java/com/xxx/typhoon/app/controller/UserController.java
@@ -1,12 +1,13 @@
 package com.xxx.typhoon.app.controller;
 
-import com.xxx.common.result.CommonResult;
 import com.xxx.typhoon.app.entity.User;
 import com.xxx.typhoon.app.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -27,5 +28,34 @@ public class UserController {
     public User login(String userName,String password){
         return userService.login(userName,password);
     }
+
+    @PostMapping("/getUserInfo")
+    public User getUserInfo(String userId){
+        return userService.getUserInfo(Long.valueOf(userId));
+    }
+
+    @PostMapping("/getUserList")
+    public List<User> getUserList(String userId){
+        return userService.getUserList(Long.valueOf(userId));
+    }
+
+
+    @PostMapping("/login")
+    public Integer login(String userId){
+        return userService.adminDeleteUser(Long.valueOf(userId));
+    }
+
+    @PostMapping("/admin/updateUserAuth")
+    public Integer adminUpdateUserAuth(String userId){
+        return userService.adminUpdateUserAuth(Long.valueOf(userId));
+    }
+
+    @PostMapping("/updatePassword")
+    public Integer updatePassword(String userId,String password){
+        return userService.updatePassword(Long.valueOf(userId),password);
+    }
+
+
+
 
 }
